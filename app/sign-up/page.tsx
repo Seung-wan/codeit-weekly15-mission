a452bd5547b28d6254b555aef980b1429d5033a5
Seung-wan <tmddhks0104@gmail.com>
2024-04-10 02:58:26 +0900

    유효한 회원가입 형식의 경우 “/api/sign-up” POST 요청하고 성공 응답을 받으면 “/folder”로 이동하나요?

--- app/sign-up/page.tsx
+++ app/sign-up/page.tsx
@@ -28,24 +28,25 @@ export default function SignUpPage() {
 
   const onSubmit = async () => {
     try {
-      // 이메일 중복 확인 api라고 가정
-      const result = await new Promise((_, reject) => {
-        setTimeout(() => {
-          reject(new Error('이미 존재하는 계정입니다.'));
-        }, 1000);
-      });
-
-      // const result = (await new Promise((resolve) => {
+      // // 이메일 중복 확인 api라고 가정
+      // const result = await new Promise((_, reject) => {
       //   setTimeout(() => {
-      //     resolve({
-      //       status: '성공',
-      //     });
+      //     reject(new Error('이미 존재하는 계정입니다.'));
       //   }, 1000);
-      // })) as { status: string };
+      // });
 
-      // if (result.status === '성공') {
-      //   push(ROUTE_PATHS.FOLDER);
-      // }
+      // 회훤가입 api라고 가정
+      const result = (await new Promise((resolve) => {
+        setTimeout(() => {
+          resolve({
+            status: '성공',
+          });
+        }, 1000);
+      })) as { status: string };
+
+      if (result.status === '성공') {
+        push(ROUTE_PATHS.FOLDER);
+      }
     } catch (error) {
       setError('email', {
         message: '다른 이메일을 입력해주세요. (회원가입 실패시 에러메시지)',
